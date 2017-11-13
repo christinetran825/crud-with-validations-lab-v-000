8f18dc9d048aa928f6d1bdc696c7dd2758f12dab
Christine Tran <christinetran825@gmail.com>
2017-11-13 23:42:24 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,14 +4,14 @@ class Song < ActiveRecord::Base
 
   validates :title, uniqueness: { scope: :release_year, message: "must be unique per artist per year"}
   validates :released, inclusion: { in: [true, false] }
-  
-  def release_info
-    if self.released && !self.release_year
-      errors.add(:release_year, "A Song needs a release year")
-    else self.release_year && self.release_year > Date.today.year
-      errors.add(:release_year, "release year can't be from the future")
-    end
-  end
+
+  # def release_info
+  #   if self.released && !self.release_year
+  #     errors.add(:release_year, "A Song needs a release year")
+  #   else self.release_year && self.release_year > Date.today.year
+  #     errors.add(:release_year, "release year can't be from the future")
+  #   end
+  # end
 
   with_options if: :released? do |song|
     song.validates :release_year, presence: true
