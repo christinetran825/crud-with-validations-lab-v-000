c3b92c181538ea0fdbcbe1fe3f36b9488cdf24fd
Christine Tran <christinetran825@gmail.com>
2017-11-13 23:44:16 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,6 +15,7 @@ class Song < ActiveRecord::Base
   with_options if: :released? do |song|
     song.validates :release_year, presence: true
     song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
 
   def released?
     released
