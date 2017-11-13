4dab94dc6dbac0ff4d61278c6203f589928cca50
Christine Tran <christinetran825@gmail.com>
2017-11-13 23:33:13 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
 
   validates :title, uniqueness: { scope: :release_year, message: "must be unique per artist per year"}
-  validates :released, inclusion: { in: [true, false] }
+  validate :release_info
 
 
   def release_info
